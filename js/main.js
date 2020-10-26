778fdc18d9ef12632ac2fdb67422e130e5fcdb1a
daraadenipekun97 <daraadenipekun97@gmail.com>
2020-10-26 14:52:25 +0100

    portfolio

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let tooglebars = document.querySelector("#bars");
-let menujs = document.querySelector(".nav_links");
-
-
-tooglebars.addEventListener("click", showhide)
-function showhide() {
-    menujs.classList.toggle('nav-active');
-    
-}
